6cabaadffac13aecc82048d19c26409a728ddf0c
Juni E. J <jujoa007@osloskolen.no>
2023-01-20 13:24:59 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<style type="text/scss">
+    :root {
+        --grayBG: #404040;
+    }
+
+    .centerBG {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 100vw;
+        height: 100vh;
+
+        background-color: var(--grayBG);
+    }
+</style>
+
+
+<div class="centerBG">
+    <button type="button" class="btn btn-primary">Primary</button>
+</div>
+
